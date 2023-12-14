09305ad5a30012d1208a2e25eadd89c0d8de47a4
Ekaterina Egorova <ekaterinan.egorova@gmail.com>
2023-12-14 08:36:43 -0500

    add admin/categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,11 @@
 class CategoriesController < ApplicationController
 
+  # def index
+  #   @categories = Category.all.order(created_at: :desc)
+  # end
+
   def show
-    @category = Category.find(params[:id])
+    @category = Category.find params[:id]
     @products = @category.products.order(created_at: :desc)
   end
 
