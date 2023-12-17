698bbce622119ee034f50eacab015ad263dc0eb3
Ekaterina Egorova <ekaterinan.egorova@gmail.com>
2023-12-16 23:36:41 -0500

    change migration for usertable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,15 @@
 
 class User < ApplicationRecord
   has_secure_password
+
+  def self.authenticate_with_credentials(email, password)
+    user = User.find_by_email(email)
+    puts user.inspect
+    if user
+      user.authenticate(password)
+    else
+      nil
+    end
+  end
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_12_16_074901) do
+ActiveRecord::Schema.define(version: 2023_12_17_031837) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,9 +54,10 @@ ActiveRecord::Schema.define(version: 2023_12_16_074901) do
   end
 
   create_table "users", id: :serial, force: :cascade do |t|
-    t.string "name", limit: 255, null: false
     t.string "email", limit: 255, null: false
-    t.string "password_digest", limit: 255, null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "password_digest"
   end
 
   add_foreign_key "line_items", "orders"
